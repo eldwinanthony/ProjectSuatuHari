2015e1df6f8af7f751787c57bef364205eb8ba02
eldwinanthony <eldwinanthony@gmail.com>
2022-06-20 15:00:39 +0700

    bikin controller ama storyboard parentalguideline

---
+++ ProjectSuatuHari/ParentalGuidelines/ParentalGuidelinesViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ParentalGuidelinesViewController.swift
+//  ProjectSuatuHari
+//
+//  Created by Eldwin Anthony on 20/06/22.
+//
+
+import UIKit
+
+class ParentalGuidelinesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
