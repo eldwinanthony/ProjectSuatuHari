6005717ea2a66c9beaa6ab8971323b6a4115a946
eldwinanthony <eldwinanthony@gmail.com>
2022-06-20 18:03:44 +0700

    parental guidelines jadi!

--- ProjectSuatuHari/ParentalGuidelines/ParentalGuidelinesViewController.swift
+++ ProjectSuatuHari/ParentalGuidelines/ParentalGuidelinesViewController.swift
@@ -12,6 +12,7 @@ class ParentalGuidelinesViewController: UIViewController, UICollectionViewDelega
     @IBOutlet weak var PGCollectionView: UICollectionView!
     @IBOutlet weak var previousBtn: UIButton!
     @IBOutlet weak var nextBtn: UIButton!
+    @IBOutlet weak var pgPageControl: UIPageControl!
     
     //SET THE ARRAY OF IMAGE
     var pgimageArray = [UIImage(named: "PG1"), UIImage(named: "PG2"), UIImage(named: "PG3"), UIImage(named: "PG4"), UIImage(named: "PG5")]
@@ -26,10 +27,13 @@ class ParentalGuidelinesViewController: UIViewController, UICollectionViewDelega
         
         previousBtn.isHidden = true
         
+        pgPageControl.numberOfPages = pgimageArray.count
        
     }
     
-    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: collectionView.frame.height)
+    }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
         return 0
@@ -46,6 +50,10 @@ class ParentalGuidelinesViewController: UIViewController, UICollectionViewDelega
         return cell
     }
     
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        pgPageControl.currentPage = currentCellIndex
+    }
+    
     
     @IBAction func nextPressed(_ sender: Any) {
         currentCellIndex += 1
@@ -55,27 +63,27 @@ class ParentalGuidelinesViewController: UIViewController, UICollectionViewDelega
         }
         
         if currentCellIndex < pgimageArray.count{
-        PGCollectionView.scrollToItem(at: IndexPath(item: currentCellIndex, section: 0), at: .centeredHorizontally, animated: true)
+            PGCollectionView.scrollToItem(at: IndexPath(item: currentCellIndex, section: 0), at: .centeredHorizontally, animated: true)
             previousBtn.isHidden = false
-            }
+        }
     }
     
     
     @IBAction func previousPressed(_ sender: Any) {
+        
         currentCellIndex -= 1
         
-        if currentCellIndex != pgimageArray.count - 1{
-            nextBtn.isHidden = false
+        if currentCellIndex != pgimageArray.count - 1 {
+            nextBtn.isHidden =  false
         }
         
-        if currentCellIndex == 0 {
+        if currentCellIndex == 0{
             previousBtn.isHidden = true
         }
         
         if currentCellIndex < pgimageArray.count{
             PGCollectionView.scrollToItem(at: IndexPath(item: currentCellIndex, section: 0), at: .centeredHorizontally, animated: true)
-            }
-    }
-    
+        }
 
+    }
 }
