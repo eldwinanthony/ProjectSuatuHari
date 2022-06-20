6a1c3574ab17c9223fb5f7ced4820b8c1530026e
eldwinanthony <eldwinanthony@gmail.com>
2022-06-20 15:19:41 +0700

    make cell controller and add collection to storyboard

---
+++ ProjectSuatuHari/ParentalGuidelines/PGCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  PGCollectionViewCell.swift
+//  ProjectSuatuHari
+//
+//  Created by Eldwin Anthony on 20/06/22.
+//
+
+import UIKit
+
+class PGCollectionViewCell: UICollectionViewCell {
+    
+}
