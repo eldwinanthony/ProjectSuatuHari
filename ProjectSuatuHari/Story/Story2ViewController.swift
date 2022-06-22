7638b30e07ac428dd380a352bcd1527489d86073
eldwinanthony <eldwinanthony@gmail.com>
2022-06-22 19:51:46 +0700

    ui scene 2

--- ProjectSuatuHari/Story/Story2ViewController.swift
+++ ProjectSuatuHari/Story/Story2ViewController.swift
@@ -7,14 +7,38 @@
 
 import UIKit
 
-class Story6ViewController: UIViewController {
+class Story2ViewController: UIViewController {
 
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    @IBOutlet weak var button4: UIButton!
+    @IBOutlet weak var button5: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        button2.layer.cornerRadius = button2.frame.size.width/2
+        button2.clipsToBounds = true
+        
+        button3.layer.cornerRadius = button2.frame.size.width/2
+        button3.clipsToBounds = true
+        
+        button4.layer.cornerRadius = button2.frame.size.width/2
+        button4.clipsToBounds = true
+        
+        button5.layer.cornerRadius = button2.frame.size.width/2
+        button5.clipsToBounds = true
+        
+        
+    }
+
+    @IBAction func prevBtnPressed(_ sender: Any) {
+        playButtonSound()
     }
     
-
-
+    @IBAction func button4Pressed(_ sender: Any) {
+        playNextActivitySound()
+        button4.backgroundColor = UIColor.green
+    }
 }
+
